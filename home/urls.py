1cc3356df5de19edb86580f9469d45bc217bb193
Nijal Maharjan <maharjannijal7@gmail.com>
2025-12-12 19:34:43 +0545

    Projectv2

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home_view, name='home'), 
+    path('home/', views.login_view, name='login'),
     path('login/', views.login_view, name='login'), 
     path('logout/', views.logout_view, name='logout'),
     path('essay/', views.essay_view, name='essay'),
